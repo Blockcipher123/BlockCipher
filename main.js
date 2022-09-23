42aba3d5555421d33d679d812c3766241583ee61
Blockcipher123 <blockcipher0@gmail.com>
2022-09-23 23:09:44 +0530

    murge all the file in single file

--- main.js
+++ main.js
@@ -6,3 +6,35 @@ hamburger.addEvenListener("click", () => {
   hamburger.classList.toggle("active");
   nav-menu.classList.toggle("active");
 })
+
+
+
+party.sparkles(element);
+
+
+party.confetti(element,{
+  /**
+   * Whether the debugging mode should be enabled.
+   */
+  debug: false,
+  /**
+   * The amount of gravity to apply to particles in the scene, in pixels.
+   * Note that this value is positive by default, since the y-axis increases
+   * downwards in a DOM.
+   */
+  gravity: 800,
+  /**
+   * The z-index to place the DOM containers at.
+   */
+  zIndex: 99999,
+});
+
+
+party.settings.debug = true;
+
+
+party.confetti(element, {
+  shapes: ["square", "circle", "roundedRectangle"]
+});
+
+party.resolvableShapes["myShape"] = `<img src="myShape.png"/>`;
